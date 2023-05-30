618f25273c6d9655e61138c5fd4d3d9961d50813
Dáud <72731777+mrscaletto@users.noreply.github.com>
2023-05-30 15:31:26 +0300

    Update ram.v

--- bunch of blocks/ram.v
+++ bunch of blocks/ram.v
@@ -1,40 +1,37 @@
 `timescale 1ns / 1ps
 
 
-module single_port_ram(ram_address,HWRITE,clk,data_out);
+module single_port_ram(
 
-    input clk;
-    input [31:0] ram_address;
-    input  HWRITE; //write_enable
+    input clk,
+    input [31:0] HADDR,
+    input  HWRITE, //write_enable
     input  [31:0] HWDATA,
     //input data_in;
-    input HREADY;
     input  [31:0] HRDATA,
-  	input  [31:0] HADDR
-    
-    //output [31:0]data_out;
+  	
+  	output reg HREADY = 1
+    //reg
+    //output [31:0]data_out
+    );
 
-reg [7:0] ram_memory[31:0]; // a 32 byte ( 32*8 bit)  RAM  
-reg [5:0] address_register;
+reg [31:0] ram_memory[31:0]; // a 32 byte ( 32*8 bit)  RAM  
+
 
 
 always@(posedge clk)
+    
     begin
         if (HWRITE)
-        ram_memory[ram_address] <= HWDATA
-    else
-   HADDR <= ram_address;
+        ram_memory[HADDR] <= HWDATA;
+    end 
         
         
-always @(posedge clk)
-    begin
-        if (write_enable)  // write operation
-        ram_memory[ram_address] <= data_in;
-    else 
-  address_register <= ram_address;
-    end
 
-assign data_out = ram_memory[address_register];
+assign data_out = ram_memory[HADDR];
+
+
 
 endmodule
+
 
