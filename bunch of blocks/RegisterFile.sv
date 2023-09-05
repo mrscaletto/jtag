03d855a590e74733223e381c118507c51f8b1bbd
Dáud <72731777+mrscaletto@users.noreply.github.com>
2023-09-05 19:13:36 +0300

    RegisterFile.sv

--- bunch of blocks/RegisterFile.sv
+++ bunch of blocks/RegisterFile.sv
@@ -15,18 +15,18 @@ module RegisterFile (
 
   always @ (posedge clk) begin
     if (srst) begin
-      // Initialize all registers to value 0
+
+      
       for(i = 0; i < 31; i=i+1) begin
         register[i] <= 'h0;
       end
     end
-    // Write to registers only on condition reg_write
-    // On reg_write, write the data to the register address provided on w_addr
+    
     else if (reg_write)
       register[w_addr] <= w_data;
   end
   
-  // Read data available on 2 read ports based on 2 seperate read addresses
+
   assign r_data1 = register[r_addr1];
   
   //assign r_data2 = register[r_addr2];
